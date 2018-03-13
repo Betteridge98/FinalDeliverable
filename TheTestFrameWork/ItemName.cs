7a94e9828d0270fcafb07963cd9b070c251f715e
Sam <p16157973@my365.dmu.ac.uk>
2018-03-13 14:43:51 +0000

    started work on my test methods

---
+++ TheTestFrameWork/ItemName.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TheTestFrameWork
+{
+    [TestClass]
+    public class ItemName
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance of the class we want to create
+            
+            //test to see if it actually exists
+
+        }
+
+    }
+}
