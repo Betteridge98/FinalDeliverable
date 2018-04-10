9f49a6f6242fca3bde7311e76f95d79fb0774b66
Katie <katie@DESKTOP-MDUNNEE>
2018-04-10 17:19:29 +0100

    added more code

--- OurClassLibrary/clsStaffCollection.cs
+++ OurClassLibrary/clsStaffCollection.cs
@@ -4,7 +4,55 @@ namespace OurClassLibrary
 {
     public class clsStaffCollection
     {
-        public List<clsStaff> AllStaff { get; set; }
-        public int Count { get; set; }
+        //private data member for the Staff list
+        private List<clsStaff> mStaffList = new List<clsStaff>();
+        //public property for count
+        public int Count
+        {
+            get
+            {
+                //return the count property of the private list
+                return mStaffList.Count;
+            }
+            set
+            {
+
+            }
+        }
+
+        //public property for Staff
+        public List<clsStaff> StaffList
+        {
+            //getter sends data to requesting code
+            get
+            {
+                //return the private data member
+                return mStaffList;
+            }
+            //setter accepts data from other objects
+            set
+            {
+                //assign the incoming value to the private data member
+                mStaffList = value;
+            }
+        }
+
+        //public contstructor for the class
+        public clsStaffCollection()
+        {
+            //create an instance of the staff class to store a staff member
+            clsStaff AStaff = new clsStaff();
+            //set the staff member to Sam
+            AStaff.FirstName = "Sam";
+            //add the name to the private list of names
+            mStaffList.Add(AStaff);
+            //re initialise the AStaff object to accept a new item
+            AStaff = new clsStaff();
+            //set the name to Bob
+            AStaff.FirstName = "Bob";
+            //add the second county to the private list of staff members
+            mStaffList.Add(AStaff);
+            //the private list now contains two staff members
+        }
     }
 }
--- TheTestFrameWork/tstStaffCollection.cs
+++ TheTestFrameWork/tstStaffCollection.cs
@@ -23,7 +23,7 @@ namespace TheTestFrameWork
             //create an instance of the class we want to create
             clsStaffCollection AllStaff = new clsStaffCollection();
             //create some test data to assign to the property
-            Int32 SomeCount = 1;
+            Int32 SomeCount = 2;
             //assign the data to the property
             AllStaff.Count = SomeCount;
             //test to see that the two values are the same
@@ -47,15 +47,41 @@ namespace TheTestFrameWork
             //add the item to the test list
             TestList.Add(TestItem);
             //assign the data to the property
-            Staff.AllStaff = TestList;
+            Staff.StaffList = TestList;
             //test to see that the two values are the same 
-            Assert.AreEqual(Staff.AllStaff, TestList);
+            Assert.AreEqual(Staff.StaffList, TestList);
         }
 
         [TestMethod]
         public void  CountMatchesList()
         {
             //create an instance of the class we want to create
+            clsStaffCollection Staff = new clsStaffCollection();
+            //create some test data to assign to the property
+            //in this case the data needs to be a list of objects
+            List<clsStaff> TestList = new List<clsStaff>();
+            //add an item to the list
+            //create the item of test data 
+            clsStaff TestItem = new clsStaff();
+            //set its properties
+            TestItem.StaffNo = 1;
+            TestItem.FirstName = "Sam";
+            //add the item to the test list
+            TestList.Add(TestItem);
+            //assign the data to the property
+            Staff.StaffList = TestList;
+            //test to see that the two values are the same
+            Assert.AreEqual(Staff.Count, TestList.Count);
+
+        }
+
+        [TestMethod]
+        public void TwoStaffPresent()
+        {
+            //create an instance of the class we want to create
+            clsStaffCollection Staff = new clsStaffCollection();
+            //test to see that the two values are the same
+            Assert.AreEqual(Staff.Count, 2);
         }
     }
 }
