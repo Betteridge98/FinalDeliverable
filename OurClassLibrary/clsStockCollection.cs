11d43875d055468b47c37a33b6ea4e21f28a6f46
SamBe <SamBe@DESKTOP-D81DQPF>
2018-04-11 23:29:59 +0100

    added small features to make it work

--- OurClassLibrary/clsStockCollection.cs
+++ OurClassLibrary/clsStockCollection.cs
@@ -1,5 +1,11 @@
-﻿using System.Collections.Generic;
-using System;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
 
 namespace OurClassLibrary
 {
