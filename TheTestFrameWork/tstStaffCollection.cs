0bff7e7d4b029435ed9d8de1639ac8fad27ec608
katie <p16190495@my365.dmu.ac.uk>
2018-04-12 00:14:26 +0100

    added database stuff

--- TheTestFrameWork/tstStaffCollection.cs
+++ TheTestFrameWork/tstStaffCollection.cs
@@ -23,7 +23,7 @@ namespace TheTestFrameWork
             //create an instance of the class we want to create
             clsStaffCollection AllStaff = new clsStaffCollection();
             //create some test data to assign to the property
-            Int32 SomeCount = 2;
+            Int32 SomeCount = 1;
             //assign the data to the property
             AllStaff.Count = SomeCount;
             //test to see that the two values are the same
